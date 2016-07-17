59f84b122ade5101fa6687f703edefb4cf57d747
punzalan101 <stephen.punzalan1@gmail.com>
2016-07-17 17:06:34 +0800

    Sale Manager, Batch Manager, Sale Panel

--- BatchManager.java
+++ BatchManager.java
@@ -1,23 +1,26 @@
 package ProjectBackEnd;
 import java.sql.*;
+import java.util.ArrayList;
+
+import javax.swing.table.DefaultTableModel;
 
 import DBConnector.DBConnection;
 
 public class BatchManager {
 	
 	private DBConnection con;
-
+	
 	public BatchManager()
 	{
 		con = new DBConnection();
 	}
 	
-	public int getBatchQuantity (int productID)
+	public int getBuyingPrice (int productID)
 	{
 		PreparedStatement ps;
 		ResultSet rs;
 		
-		String sQuery = "SELECT batch_quantity "
+		String sQuery = "SELECT buying_price "
 				+ "FROM batch "
 				+ "WHERE productID = '" + productID + "';";
 		
@@ -38,13 +41,91 @@ public class BatchManager {
 			
 	}
 	
-	public void addFirstBatch(int batchquantity, double buyingprice, int expiremonth, int expiryyear)
+	public int getBatchQuantity (int productID)
+	{
+		PreparedStatement ps;
+		ResultSet rs;
+		
+		String sQuery = "SELECT SUM(batch_quantity) "
+				+ "FROM batch "
+				+ "WHERE productID = '" + productID + "';";
+		
+		try {
+			ps = con.getConnection().prepareStatement(sQuery);
+			ps.executeQuery(sQuery);
+			
+			rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				return rs.getInt(1);
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return 0;
+			
+	}
+	
+	public int getExpiryMonth (int productID)
+	{
+		PreparedStatement ps;
+		ResultSet rs;
+		
+		String sQuery = "SELECT expiry_month "
+				+ "FROM batch "
+				+ "WHERE productID = '" + productID + "';";
+		
+		try {
+			ps = con.getConnection().prepareStatement(sQuery);
+			ps.executeQuery(sQuery);
+			
+			rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				return (rs.getInt(1)+1);
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return 0;
+			
+	}
+	
+	public int getExpiryYear (int productID)
+	{
+		PreparedStatement ps;
+		ResultSet rs;
+		
+		String sQuery = "SELECT expiry_year "
+				+ "FROM batch "
+				+ "WHERE productID = '" + productID + "';";
+		
+		try {
+			ps = con.getConnection().prepareStatement(sQuery);
+			ps.executeQuery(sQuery);
+			
+			rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				return rs.getInt(1);
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return 0;
+			
+	}
+	
+	public void addBatch(int batchquantity, double buyingprice, int expiremonth, int expiryyear)
 	{
 		ProductManager getIDmanage = new ProductManager();
 		PreparedStatement ps;
 		
 		String sQuery = "INSERT INTO batch(productID, batch_quantity, expiry_month, expiry_year, entry_date, buying_price)"
-						+ "VALUES('"+ getIDmanage.getLatestProductID() +"','"+ batchquantity +"','"+ expiremonth +"','"+ expiryyear +"','CURDATE()', '"+ buyingprice + "')";
+						+ "VALUES('"+ getIDmanage.getLatestProductID() +"','"+ batchquantity +"','"+ (expiremonth+1) +"','"+ expiryyear +"', CURDATE(), '"+ buyingprice + "')";
 
 		try {
 			ps = con.getConnection().prepareStatement(sQuery);
@@ -55,13 +136,12 @@ public class BatchManager {
 		
 	}
 	
-	public void addNewBatch(double buyingprice)
+	public void restockBatch(int batchquantity, double buyingprice, int expiremonth, int expiryyear, int productID)
 	{
-		ProductManager getIDmanage = new ProductManager();
 		PreparedStatement ps;
 		
-		String sQuery = "INSERT INTO batch(productID, batch_quantity, entry_date, buying_price)"
-						+ "VALUES('"+ getIDmanage.getLatestProductID() +"','1', 'CURDATE()', '"+ buyingprice + "')";
+		String sQuery = "INSERT INTO batch(productID, batch_quantity, expiry_month, expiry_year, entry_date, buying_price) "
+					+ "VALUES('" + productID + "', '" + batchquantity + "','"+ (expiremonth+1) +"','"+ expiryyear +"', CURDATE(), '"+ buyingprice + "') ";
 
 		try {
 			ps = con.getConnection().prepareStatement(sQuery);
@@ -70,39 +150,84 @@ public class BatchManager {
 			e.printStackTrace();
 		}
 		
+	}
+
+    public void changeBatchQtyToZero(int batchID){
+        PreparedStatement ps;
+        String sQuery = "UPDATE batch "
+				+ "SET batch_quantity = '0'"
+				+ "WHERE batchID = "+ batchID +";";
+        try {
+		ps = con.getConnection().prepareStatement(sQuery);
+		ps.executeUpdate(sQuery);
+	} catch(SQLException e) {
+		e.printStackTrace();
+        }
+    }
+    
+    public ArrayList<Integer> getBatchIDofProductList(int productID){
+            ArrayList<Integer> BatchIDofProductList = new ArrayList<>();
+            PreparedStatement ps;
+	ResultSet rs;
+	String sQuery = "SELECT batchID "
+			+ "FROM batch "
+			+ "WHERE productID = '" + productID + "';";
+	
+	try {
+		ps = con.getConnection().prepareStatement(sQuery);
+		ps.executeQuery(sQuery);
+		
+		rs = ps.executeQuery();
+		
+		while(rs.next()) {
+			BatchIDofProductList.add(rs.getInt(1));
+		}
+                    return BatchIDofProductList;
+	} catch(SQLException e) {
+		e.printStackTrace();
 	}
 	
-	public void addBatch(int productID, double buyingprice)
-	{
-		PreparedStatement ps;
+	return null;
+    }
+    
+    public int getEachBatchQuantity (int batchID)
+{
+	PreparedStatement ps;
+	ResultSet rs;
+	
+	String sQuery = "SELECT batch_quantity "
+			+ "FROM batch "
+			+ "WHERE batchID = '" + batchID + "';";
+	
+	try {
+		ps = con.getConnection().prepareStatement(sQuery);
+		ps.executeQuery(sQuery);
 		
-		String sQuery = "INSERT INTO batch(productID, entry_date, buying_price)"
-						+ "VALUES('"+ productID +"', 'CURDATE()', '"+ buyingprice + "')";
-
-		try {
-			ps = con.getConnection().prepareStatement(sQuery);
-			ps.executeUpdate(sQuery);
-		} catch(SQLException e) {
-			e.printStackTrace();
+		rs = ps.executeQuery();
+		
+		if(rs.next()) {
+			return rs.getInt(1);
 		}
-		
+	} catch(SQLException e) {
+		e.printStackTrace();
 	}
 	
-
-	public void restockBatch(int batchquantity, double buyingprice, int productID)
-	{
-		PreparedStatement ps;
-		String sQuery = "UPDATE batch "
-					+ "SET batch_quantity = '" + (getBatchQuantity(productID) + batchquantity) + "', buying_price = '" + buyingprice + "' "
-					+ "WHERE productID = "+ productID +";";
-
-		try {
-			ps = con.getConnection().prepareStatement(sQuery);
-			ps.executeUpdate(sQuery);
-		} catch(SQLException e) {
-			e.printStackTrace();
-		}
+	return 0;
 		
-	}
-
 }
+    
+    public void subtractBatchQty(int batchID, int batchQty, int QtySold){
+        PreparedStatement ps;
+        int difference = batchQty - QtySold;
+        String sQuery = "UPDATE batch "
+				+ "SET batch_quantity = '"+ difference +"'"
+				+ "WHERE batchID = "+ batchID +";";
+        try {
+		ps = con.getConnection().prepareStatement(sQuery);
+		ps.executeUpdate(sQuery);
+	} catch(SQLException e) {
+		e.printStackTrace();
+        }
+    }
+}
+
--- SalePanel.java
+++ SalePanel.java
@@ -1,5 +1,7 @@
 package ProjectFrontEnd;
 
+import ProjectBackEnd.ProductManager;
+import ProjectBackEnd.SaleManager;
 import java.awt.*;
 
 import javax.swing.*;
@@ -7,7 +9,13 @@ import net.miginfocom.swing.MigLayout;
 import javax.swing.border.TitledBorder;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import javax.swing.border.LineBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 
 public class SalePanel extends JPanel implements ActionListener {
@@ -21,21 +29,33 @@ public class SalePanel extends JPanel implements ActionListener {
 	private JScrollPane scrollPaneSelectItem = new JScrollPane();
 	private JTable tblCart = new JTable();
 	private JButton btnAddToCart = new JButton("Add to Cart");
-	private JLabel lblAddToCartFeedback = new JLabel("User warning message");
+	private JLabel lblAddToCartFeedback = new JLabel(" ");
 	private JPanel pnlCart = new JPanel();
 	private JLabel lblHelpCart = new JLabel("Select and item and click remove to delete it from the cart");
 	private JButton btnRemove = new JButton("Remove");
 	private JScrollPane scrollPaneCart = new JScrollPane();
 	private JLabel lblSalesDate = new JLabel("Sales Date:");
 	private JLabel lblTotal = new JLabel("Total: ");
-	private JLabel lblTotalValue = new JLabel("<Amount>");
+	private JLabel lblTotalValue = new JLabel("0");
 	private JButton btnRecord = new JButton("Record");
-	private JLabel lblFeedbackCart = new JLabel("User Prompt Message");
+	private JLabel lblFeedbackCart = new JLabel(" ");
 	private JComboBox cboSalesDateMonth = new JComboBox();
 	private JComboBox cboSalesDateDay = new JComboBox();
 	private JComboBox cboSalesDateYear = new JComboBox();
 	
+        private ProductManager productManage = new ProductManager();
+        private SaleManager saleManage = new SaleManager();
+        private DefaultTableModel tm = new DefaultTableModel();
+        private ArrayList<String> prodNameList = new ArrayList<String>();
+        private ArrayList<Integer> prodQtyList = new ArrayList<Integer>();
+        private int sum = 0;
+        private int totalQty = 0;
+        private int sellingprice;
+            
+        
 	public SalePanel() {
+                tm.setColumnIdentifiers(new String[] {"Item Name", "Quantity", "Selling Price", "Subtotal"});
+            
 		setBackground(Color.WHITE);
 		setLayout(new MigLayout("", "[300][grow]", "[grow]"));
 		
@@ -58,14 +78,7 @@ public class SalePanel extends JPanel implements ActionListener {
 				btnSearch.setFont(new Font("Segoe UI", Font.PLAIN, 11));
 		
 		tblSaleSearch.setFont(new Font("Segoe UI", Font.PLAIN, 11));
-		tblSaleSearch.setModel(new DefaultTableModel(
-			new Object[][] {
-				{null, null},
-			},
-			new String[] {
-				"Item Name", "Quantity"
-			}
-		));
+		tblSaleSearch.setModel(productManage.viewProducts());
 		tblSaleSearch.getColumnModel().getColumn(1).setPreferredWidth(65);
 		tblSaleSearch.getColumnModel().getColumn(1).setMinWidth(65);
 		tblSaleSearch.getColumnModel().getColumn(1).setMaxWidth(65);
@@ -76,6 +89,8 @@ public class SalePanel extends JPanel implements ActionListener {
 		btnAddToCart.setFont(new Font("Segoe UI", Font.PLAIN, 11));
 		btnAddToCart.setMinimumSize(new Dimension(100, 35));
 		btnAddToCart.setBackground(new Color(51, 255, 153));
+                btnAddToCart.addActionListener(this);
+                
 		
 		lblAddToCartFeedback.setFont(new Font("Segoe UI", Font.PLAIN, 11));
 		
@@ -83,7 +98,7 @@ public class SalePanel extends JPanel implements ActionListener {
 		
 		txtQuantity.setFont(new Font("Segoe UI", Font.PLAIN, 11));
 		txtQuantity.setColumns(10);
-		
+                
 		pnlAddToCart.add(scrollPaneSelectItem, "cell 0 1 2 1,grow");
 		pnlAddToCart.add(lblQuantity, "cell 0 2,alignx right,growy");
 		pnlAddToCart.add(btnAddToCart, "cell 0 3 2 1,alignx right");
@@ -99,18 +114,10 @@ public class SalePanel extends JPanel implements ActionListener {
 		btnRemove.setForeground(Color.WHITE);
 		btnRemove.setFont(new Font("Segoe UI", Font.PLAIN, 11));
 		btnRemove.setBackground(Color.RED);
+                btnRemove.addActionListener(this);
 		
 		tblCart.setFont(new Font("Segoe UI", Font.PLAIN, 11));
-		tblCart.setModel(new DefaultTableModel(
-			new Object[][] {
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-			},
-			new String[] {
-				"Item Name", "Quantity", "Selling Price", "Subtotal"
-			}
-		));
+		tblCart.setModel(tm);
 		scrollPaneCart.setViewportView(tblCart);
 		
 		lblTotal.setFont(new Font("Segoe UI", Font.PLAIN, 11));
@@ -135,6 +142,7 @@ public class SalePanel extends JPanel implements ActionListener {
 		btnRecord.setFont(new Font("Segoe UI", Font.PLAIN, 11));
 		btnRecord.setMinimumSize(new Dimension(100, 35));
 		btnRecord.setBackground(new Color(0, 204, 0));
+                btnRecord.addActionListener(this);
 		
 		pnlCart.add(lblHelpCart, "cell 0 0");
 		pnlCart.add(btnRemove, "cell 1 0,alignx right");
@@ -150,9 +158,120 @@ public class SalePanel extends JPanel implements ActionListener {
 		
 		add(pnlAddToCart, "cell 0 0,alignx left,growy");
 		add(pnlCart, "cell 1 0,grow");
+                
+                txtQuantity.setEditable(false);
+                btnAddToCart.setEnabled(true);
+                selectTableField();
 	}
-
+        
+        public void selectTableField()
+	{
+		tblSaleSearch.getSelectionModel().addListSelectionListener(new ListSelectionListener()
+		{
+			
+			@Override
+			public void valueChanged(ListSelectionEvent event) {
+				txtQuantity.setEditable(true);			}
+                                
+			
+		});
+                
+	}
+        
+        
+        public void searchTable()
+	{
+		if(txtSearch.getText().toString().equals(""))
+			tblSaleSearch.setModel(productManage.viewProducts());
+                else{
+                tblSaleSearch.clearSelection();
+		tblSaleSearch.setModel(productManage.searchProduct(txtSearch.getText()));
+                }
+	}
+                
 	public void actionPerformed(ActionEvent e) {
-		
+            sellingprice=0;
+                if(e.getSource().equals(btnSearch)){
+                   searchTable();
+                }
+		if(e.getSource().equals(btnAddToCart)){
+                    lblAddToCartFeedback.setText(" ");
+                   
+                    if(txtQuantity.getText().length() >= 1){
+                       
+                        if(Integer.parseInt(txtQuantity.getText()) <= Integer.parseInt(String.valueOf(tblSaleSearch.getModel().getValueAt(tblSaleSearch.getSelectedRow(), 1)))){
+                            
+                            if(saleManage.getDuplicate(prodNameList, String.valueOf(tblSaleSearch.getModel().getValueAt(tblSaleSearch.getSelectedRow(), 0))) == 999){
+                                sellingprice = productManage.getSellingPrice(String.valueOf(tblSaleSearch.getModel().getValueAt(tblSaleSearch.getSelectedRow(), 0)));
+                                totalQty += Integer.parseInt(txtQuantity.getText());
+                      
+                                sum += saleManage.getSubtotal(Integer.parseInt(txtQuantity.getText()), sellingprice);
+                                tm.addRow(new Object[]{tblSaleSearch.getModel().getValueAt(tblSaleSearch.getSelectedRow(), 0), 
+                                               txtQuantity.getText(), 
+                                               sellingprice, 
+                                               saleManage.getSubtotal(Integer.parseInt(txtQuantity.getText()), sellingprice)
+                                              });                        
+                        
+                                prodNameList.add(String.valueOf(tblSaleSearch.getModel().getValueAt(tblSaleSearch.getSelectedRow(), 0)));
+                                prodQtyList.add(Integer.parseInt(txtQuantity.getText()));
+                            }
+                            else{
+                                int index;
+                                index = saleManage.getDuplicate(prodNameList, String.valueOf(tblSaleSearch.getModel().getValueAt(tblSaleSearch.getSelectedRow(), 0)));
+                                System.out.println(prodQtyList.get(index)+Integer.parseInt(txtQuantity.getText()));
+                                if((prodQtyList.get(index)+Integer.parseInt(txtQuantity.getText())) <= Integer.parseInt(String.valueOf(tblSaleSearch.getModel().getValueAt(tblSaleSearch.getSelectedRow(), 1)))){
+                                sellingprice = productManage.getSellingPrice(String.valueOf(tblSaleSearch.getModel().getValueAt(tblSaleSearch.getSelectedRow(), 0)));
+                                sum += saleManage.getSubtotal(Integer.parseInt(txtQuantity.getText()), sellingprice);
+                                totalQty += Integer.parseInt(txtQuantity.getText());
+                                int newQty = prodQtyList.get(index)+Integer.parseInt(txtQuantity.getText());
+                                tm.setValueAt(newQty,index ,1);
+                                tm.setValueAt(saleManage.getSubtotal(newQty, sellingprice), index, 3);
+                                prodQtyList.set(index, (prodQtyList.get(index)+Integer.parseInt(txtQuantity.getText())));
+                                System.out.println("Valid Qty");
+                                }
+                                else lblAddToCartFeedback.setText("It exceeded the quantity of the product!");
+                            }
+                        }
+                        else lblAddToCartFeedback.setText("Invalid Quantity!");
+                    }
+                    else lblAddToCartFeedback.setText("Please fill in all fields!");
+                    
+                tblSaleSearch.clearSelection();
+                txtQuantity.setText("");
+                txtQuantity.setEditable(false);
+                btnAddToCart.setEnabled(true);
+                }
+                if(e.getSource().equals(btnRemove)){
+                        if(tblCart.getSelectedRow() != -1){
+                        sum -= Integer.parseInt(String.valueOf(tblCart.getModel().getValueAt(tblCart.getSelectedRow(), 3)));
+                        totalQty -= Integer.parseInt(String.valueOf(tblCart.getModel().getValueAt(tblCart.getSelectedRow(), 1)));
+                        prodNameList.remove(tblCart.getSelectedRow());
+                        prodQtyList.remove(tblCart.getSelectedRow());
+                        tm.removeRow(tblCart.getSelectedRow());
+                        }
+                        tblCart.clearSelection();
+                }
+                if(e.getSource().equals(btnRecord)){
+                    if(tblCart.getRowCount() != 0){
+                        StringBuilder preferredDate = new StringBuilder(); 
+                        preferredDate.append(cboSalesDateYear.getSelectedItem().toString());
+                        preferredDate.append("-");
+                        preferredDate.append(String.valueOf(cboSalesDateMonth.getSelectedIndex()+1));
+                        preferredDate.append("-");
+                        preferredDate.append(String.valueOf(cboSalesDateDay.getSelectedIndex()+1));
+                        saleManage.recordTransaction(totalQty, sum, preferredDate.toString());
+                        saleManage.deductProductBatchQty(prodNameList, prodQtyList);
+                        saleManage.recordLineItem(prodNameList);
+                        prodNameList.clear();
+                        prodQtyList.clear();
+                        totalQty = 0;
+                        sum = 0;
+                        tm.setRowCount(0);
+                        tblSaleSearch.setModel(productManage.viewProducts());
+                        lblFeedbackCart.setText("Recorded Successfully!");
+                    }
+                }
+                
+            lblTotalValue.setText(String.valueOf(sum));
 	}
 }
