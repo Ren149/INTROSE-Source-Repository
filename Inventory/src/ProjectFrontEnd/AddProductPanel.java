98c3661bd1c7ca2a5c6cf2146abb0c627372d263
Nico Rapisora <Nico Rapisora>
2016-07-29 14:46:37 +0800

    Connected Alex's code onto Product List Panel

--- Inventory/src/ProjectFrontEnd/AddProductPanel.java
+++ Inventory/src/ProjectFrontEnd/AddProductPanel.java
@@ -1,4 +1,4 @@
-package GUI;
+package ProjectFrontEnd;
 
 import java.awt.EventQueue;
 
@@ -12,79 +12,139 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Color;
+import java.awt.Component;
+import javax.swing.Box;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
-public class AddProductPanel extends JPanel implements ActionListener{
-	//Product Name
-	private JLabel lblProductName = new JLabel("1. Enter product name:");
+public class AddProductPanel extends JFrame implements ActionListener{
+	
+	private JLabel lblProductName = new JLabel("Product Name:");
+	private JLabel lblBuyingPrice = new JLabel("Buying Price:");
+	private JLabel lblSellingPrice = new JLabel("Selling Price:");
+	private JLabel lblQuantity = new JLabel("Quantity:");
+	private JLabel lblProductNameError = new JLabel("");
+	private JLabel lblBuyingPriceError = new JLabel("");
+	private JLabel lblSellingPriceError = new JLabel("");
+	private JLabel lblQuantityError = new JLabel("");
+	private JLabel lblTitle = new JLabel("Add Product");
+	private JLabel lblExpiryDate = new JLabel("Expiry Date:");
 	private JTextField txtProductName = new JTextField();
-	
-	//Buy Price
-	private JLabel lblBuyingPrice = new JLabel("2. Enter buying price:");
 	private JTextField txtBuyingPrice = new JTextField();
-	
-	//Sell Price
-	private JLabel lblSellingPrice = new JLabel("3. Enter selling price:");
-	private JTextField txtSellingPrice;
-	
-	//Product Quantity
-	private JLabel lblProductQuantity = new JLabel("4. Enter quantity of product:");
-	private JTextField txtProductQuantity = new JTextField();
-	
-	//Add Product
-	private JLabel lblProductStatus = new JLabel("<Item Status>");
-	private JButton btnAdd = new JButton("Add");;
-	
-
+	private JTextField txtSellingPrice = new JTextField();
+	private JTextField txtQuantity = new JTextField();
+	private JButton btnAdd = new JButton("Add");
+	private Component verticalStrut = Box.createVerticalStrut(20);
+	private JComboBox cboExpiryMonth = new JComboBox();
+	private JComboBox cboExpiryYear = new JComboBox();
+	private JPanel panel = new JPanel();
 	
 	public AddProductPanel() {
-		setBackground(new Color(255, 255, 255));
-		setBounds(100, 100, 366, 289);
-		setLayout(new MigLayout("", "[][][]", "[][][][][][][][][][]"));
+		setTitle("Add Product");
 		
-		//Product Name
+		getContentPane().setBackground(new Color(255, 255, 255));
+		getContentPane().setBounds(100, 100, 366, 289);
+		getContentPane().setLayout(new MigLayout("", "[]", "[]"));
+		getContentPane().add(panel, "cell 0 0,grow");
+		
+		lblTitle.setFont(new Font("Segoe UI", Font.PLAIN, 16));
+		
 		lblProductName.setFont(new Font("Segoe UI", Font.PLAIN, 11));
-		add(lblProductName, "cell 0 0");
-		setFont(new Font("Segoe UI", Font.PLAIN, 11));
-		add(txtProductName, "cell 2 0,growx");
-		txtProductName.setColumns(10);
 		
-		//Buy Price
-		lblBuyingPrice.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		add(lblBuyingPrice, "cell 0 2");
+		txtProductName.setFont(new Font("Segoe UI", Font.PLAIN, 11));
+		txtProductName.setColumns(20);
+		
+		lblBuyingPrice.setFont(new Font("Segoe UI", Font.PLAIN, 11));
+				
 		txtBuyingPrice.setFont(new Font("Segoe UI", Font.PLAIN, 11));
-		add(txtBuyingPrice, "cell 2 2,growx");
 		txtBuyingPrice.setColumns(10);
 		
-		//Sell Price
 		lblSellingPrice.setFont(new Font("Segoe UI", Font.PLAIN, 11));
-		add(lblSellingPrice, "cell 0 4");
+	
 		txtSellingPrice.setFont(new Font("Segoe UI", Font.PLAIN, 11));
-		add(txtSellingPrice, "cell 2 4,growx");
 		txtSellingPrice.setColumns(10);
 		
-		//Product Quantity
-		lblProductQuantity.setFont(new Font("Segoe UI", Font.PLAIN, 11));
-		add(lblProductQuantity, "cell 0 6");
-		txtProductQuantity = new JTextField();
-		txtProductQuantity.setFont(new Font("Segoe UI", Font.PLAIN, 11));
-		add(txtProductQuantity, "cell 2 6,growx");
-		txtProductQuantity.setColumns(10);
+		lblQuantity.setFont(new Font("Segoe UI", Font.PLAIN, 11));
 		
-		//Add Product
-		lblProductStatus.setFont(new Font("Segoe UI", Font.PLAIN, 11));
-		add(lblProductStatus, "flowx,cell 2 9,aligny bottom");
+		txtQuantity.setFont(new Font("Segoe UI", Font.PLAIN, 11));
+		txtQuantity.setColumns(10);
+		
+		lblExpiryDate.setFont(new Font("Segoe UI", Font.PLAIN, 11));
+		
+		cboExpiryMonth.setFont(new Font("Segoe UI", Font.PLAIN, 11));
+		cboExpiryMonth.setModel(new DefaultComboBoxModel(new String[] {"Month", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"}));
+		
+		cboExpiryYear.setFont(new Font("Segoe UI", Font.PLAIN, 11));
+		cboExpiryYear.setModel(new DefaultComboBoxModel(new String[] {"Year"}));
+		
 		btnAdd.setForeground(new Color(255, 255, 255));
 		btnAdd.setBackground(new Color(51, 204, 0));
 		btnAdd.setFont(new Font("Segoe UI", Font.PLAIN, 11));
-		add(btnAdd, "cell 2 9,alignx right,aligny bottom");
+		btnAdd.addActionListener(this);
+		
+		panel.setBackground(Color.WHITE);
+		panel.setLayout(new MigLayout("", "[][grow]", "[][][][][][][][][][][][]"));
+		panel.add(lblTitle, "cell 0 0 2 1");
+		panel.add(lblProductName, "cell 0 1,alignx right");
+		panel.add(txtProductName, "cell 1 1");
+		panel.add(lblProductNameError, "cell 1 2");
+		panel.add(lblBuyingPrice, "cell 0 3,alignx right");
+		panel.add(txtBuyingPrice, "cell 1 3");
+		panel.add(lblBuyingPriceError, "cell 1 4,alignx left");
+		panel.add(lblSellingPrice, "cell 0 5,alignx right");
+		panel.add(txtSellingPrice, "cell 1 5");
+		panel.add(lblSellingPriceError, "cell 1 6");
+		panel.add(lblQuantity, "cell 0 7,alignx right");
+		panel.add(txtQuantity, "cell 1 7");
+		panel.add(lblQuantityError, "cell 1 8");
+		panel.add(lblExpiryDate, "cell 0 9,alignx right");
+		panel.add(cboExpiryMonth, "flowx,cell 1 9,alignx left");
+		panel.add(verticalStrut, "cell 1 10");
+		panel.add(btnAdd, "cell 1 11,alignx right,aligny bottom");
+		panel.add(cboExpiryYear, "cell 1 9");
+		
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
 	}
 
-
-
+	public boolean allValidInputs() {
+		//insert code for error correction
+		return true;
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+		if(allValidInputs()) {
+			//insert code to add data to database			
+			dispose();
+		}
+		else {
+			//insert code to set error message text
+		}
 	}
-
+	
+	public String getProductName() {
+		return txtProductName.getText();
+	}
+	
+	public float getBuyingPrice() {
+		return Float.parseFloat(txtBuyingPrice.getText());
+	}
+	
+	public float getSellingPrice() {
+		return Float.parseFloat(txtSellingPrice.getText());
+	}
+	
+	public int getQuantity() {
+		return Integer.parseInt(txtQuantity.getText());
+	}
+	
+	public int getExpiryMonth() {
+		return cboExpiryMonth.getSelectedIndex();
+	}
+	
+	public int getExpiryYear() {
+		return Integer.parseInt(cboExpiryYear.getSelectedItem().toString());
+	}
 }
--- Inventory/src/ProjectFrontEnd/ProductListPanel.java
+++ Inventory/src/ProjectFrontEnd/ProductListPanel.java
@@ -7,12 +7,16 @@ import java.awt.event.ActionListener;
 import java.util.Date;
 
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.border.EtchedBorder;
+import javax.swing.table.DefaultTableModel;
 
 import ProjectBackEnd.ProductManager;
 import net.miginfocom.swing.MigLayout;
@@ -20,60 +24,124 @@ import net.miginfocom.swing.MigLayout;
 public class ProductListPanel extends JPanel implements ActionListener {
 	private int rowCount = 0;
 
-	private JLabel lblProductListDate = new JLabel("Product List as of " + new Date());
+	private JLabel lblProductListTitle = new JLabel("Product List");
 	private JLabel lblItemCount = new JLabel("Displaying " + rowCount + " items");
 	private JTextField txtProductSearch = new JTextField();
 	private JButton btnSearch = new JButton("Search");
 	private JTable tblProductListTable;
-	private JScrollPane scrollPane = new JScrollPane();
+	private JScrollPane scrProduct = new JScrollPane();
 	private ProductManager productManage = new ProductManager();
-	private final JButton btnClear = new JButton("Clear");
-	private final JPanel panel = new JPanel();
+	private final JScrollPane scrBatch = new JScrollPane();
+	private final JTable table = new JTable();
+	private final JButton btnRestock = new JButton("Restock");
+	private final JButton btnAddProduct = new JButton("Add Product");
+	private final JLabel lblBatches = new JLabel("Batches");
+	private final JPanel panel_1 = new JPanel();
+	private final JPanel panel_2 = new JPanel();
+	private final JButton btnDiscontinue = new JButton("Discontinue");
+	private final JSeparator separator = new JSeparator();
+	private final JLabel lblTimestamp = new JLabel("New label");
 	
 	public ProductListPanel() {
-		setBackground(new Color(245, 245, 245));
-		setLayout(new MigLayout("", "[grow]", "[grow]"));
-
-		panel.setBackground(new Color(255, 255, 255));
-		panel.setLayout(new MigLayout("", "[483.00][grow]", "[][][grow][]"));
+		setBackground(Color.WHITE);
+		setLayout(new MigLayout("", "[grow][][:350px:350px]", "[grow]"));
+		add(panel_2, "cell 0 0,grow");
+		panel_2.setBackground(Color.WHITE);
+		panel_2.setLayout(new MigLayout("", "[][grow]", "[][][][grow][][]"));
+		panel_2.add(lblProductListTitle, "cell 0 0");
 		
-		lblProductListDate.setFont(new Font("Segoe UI", Font.PLAIN, 16));
+		lblProductListTitle.setFont(new Font("Segoe UI", Font.PLAIN, 16));
+		lblTimestamp.setFont(new Font("Segoe UI", Font.PLAIN, 13));
+		
+		panel_2.add(lblTimestamp, "cell 0 1 2 1,alignx left");
+		panel_2.add(txtProductSearch, "flowx,cell 0 2,growy");
 		
 		txtProductSearch.setFont(new Font("Segoe UI", Font.PLAIN, 11));
-		txtProductSearch.setColumns(10);
+		txtProductSearch.setColumns(20);
+		panel_2.add(btnSearch, "cell 1 2,alignx left,growy");
 		
-		btnSearch.setForeground(Color.WHITE);
+		btnSearch.setForeground(Color.DARK_GRAY);
 		btnSearch.setFont(new Font("Segoe UI", Font.PLAIN, 11));
-		btnSearch.setBackground(new Color(51, 153, 0));
+		btnSearch.setBackground(Color.WHITE);
 		btnSearch.addActionListener(this);
 		
 		tblProductListTable = new JTable();
-
-		scrollPane.setViewportView(tblProductListTable);
+				panel_2.add(scrProduct, "cell 0 3 2 1,grow");
 		
-		tblProductListTable.setFont(new Font("Segoe UI", Font.PLAIN, 11));
-		tblProductListTable.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+				scrProduct.setViewportView(tblProductListTable);
+				
+				tblProductListTable.setFont(new Font("Segoe UI", Font.PLAIN, 11));
+				tblProductListTable.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+				panel_2.add(lblItemCount, "cell 0 4,aligny center");
+				
+				lblItemCount.setFont(new Font("Segoe UI", Font.PLAIN, 11));
+				btnDiscontinue.setFont(new Font("Segoe UI", Font.PLAIN, 11));
+				btnDiscontinue.setBackground(Color.WHITE);
+				btnDiscontinue.setForeground(Color.DARK_GRAY);
+				
+				panel_2.add(btnDiscontinue, "flowx,cell 1 4,alignx right,growy");
+				panel_2.add(btnRestock, "cell 1 4,alignx right,growy");
+				btnRestock.setForeground(Color.DARK_GRAY);
+				btnRestock.setFont(new Font("Segoe UI", Font.PLAIN, 11));
+				btnRestock.setBackground(Color.WHITE);
+				panel_2.add(btnAddProduct, "cell 1 4,alignx right,growy");
+				btnAddProduct.setFont(new Font("Segoe UI", Font.PLAIN, 11));
+				btnAddProduct.setForeground(Color.DARK_GRAY);
+				btnAddProduct.setBackground(new Color(0, 128, 0));
+				btnAddProduct.addActionListener(this);
+				
+		separator.setForeground(new Color(211, 211, 211));
+		separator.setOrientation(SwingConstants.VERTICAL);
+		separator.setBackground(new Color(255, 255, 255));
+		
+		add(separator, "cell 1 0,alignx center,growy");
+		add(panel_1, "cell 2 0,growy");
+		panel_1.setBackground(Color.WHITE);
+		panel_1.setLayout(new MigLayout("", "[]", "[][grow]"));
+		panel_1.add(lblBatches, "cell 0 0,alignx left");
+		lblBatches.setFont(new Font("Segoe UI", Font.PLAIN, 13));
+		table.setEnabled(false);
+		table.setFillsViewportHeight(true);
+		table.setFont(new Font("Segoe UI", Font.PLAIN, 11));
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null},
+			},
+			new String[] {
+				"BatchID", "Entry Date", "Buying Price", "Expiry Date"
+			}
+		));
+		table.getColumnModel().getColumn(0).setPreferredWidth(55);
+		table.getColumnModel().getColumn(0).setMinWidth(55);
+		table.getColumnModel().getColumn(1).setResizable(false);
+		table.getColumnModel().getColumn(1).setPreferredWidth(70);
+		table.getColumnModel().getColumn(1).setMinWidth(20);
+		table.getColumnModel().getColumn(2).setMinWidth(75);
+		table.getColumnModel().getColumn(3).setPreferredWidth(70);
+		table.getColumnModel().getColumn(3).setMinWidth(20);
+		
+		panel_1.add(scrBatch, "cell 0 1,alignx center,growy");
+		
+		scrBatch.setViewportView(table);
+		
 		viewTable();
-		
-		lblItemCount.setFont(new Font("Segoe UI", Font.PLAIN, 11));
-
-		btnClear.setBackground(new Color(255, 255, 255));
-		btnClear.setForeground(new Color(0, 0, 0));
-		btnClear.setFont(new Font("Segoe UI", Font.PLAIN, 11));
-		btnClear.addActionListener(this);
-		
-		panel.add(lblProductListDate, "cell 0 0 2 1");
-		panel.add(txtProductSearch, "cell 0 1,grow");
-		panel.add(btnSearch, "flowx,cell 1 1");
-		panel.add(scrollPane, "cell 0 2 2 1,grow");
-		panel.add(lblItemCount, "cell 0 3 2 1");
-		panel.add(btnClear, "cell 1 1");
-		
-		add(panel, "cell 0 0,grow");
 	}
 
 	public void viewTable() {
-		tblProductListTable.setModel(productManage.viewProductsWithPrice());
+		tblProductListTable.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Product Name", "Quantity", "Selling Price"
+			}
+		) {
+			boolean[] columnEditables = new boolean[] {
+				false, false, false
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
 		rowCount = tblProductListTable.getRowCount();
 		lblItemCount.setText("Displaying " + rowCount + " items");
 	}
@@ -91,15 +159,31 @@ public class ProductListPanel extends JPanel implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource().equals(btnSearch)) {
 			searchTableWithPrices();
-			this.repaint();
 		}
-		else if(e.getSource().equals(btnClear)) {
-			txtProductSearch.setText("");
+		else if(e.getSource().equals(btnAddProduct)) {
+			AddProductPanel frmAddProduct = new AddProductPanel();
 		}
 	}
 	
 	public void update() {
-		lblProductListDate.setText("Product List as of " + new Date());
+		Date d = new Date();
+		String timestamp = "as of ";
+		switch(d.getMonth()) {
+			case 1: timestamp += "January "; break;
+			case 2: timestamp += "February "; break;
+			case 3: timestamp += "March "; break;
+			case 4: timestamp += "April "; break;
+			case 5: timestamp += "May "; break;
+			case 6: timestamp += "June "; break;
+			case 7: timestamp += "July "; break;
+			case 8: timestamp += "August "; break;
+			case 9: timestamp += "September "; break;
+			case 10: timestamp += "October "; break;
+			case 11: timestamp += "November "; break;
+			case 12: timestamp += "December "; break;
+		}
+		timestamp += d.getDate() + ", " + (d.getYear() + 1900);
+		lblTimestamp.setText(timestamp);
 		viewTable();
 	}
 }
