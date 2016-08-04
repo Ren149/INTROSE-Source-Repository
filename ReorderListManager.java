0098912bcc3eb4d7f5debd5154ebc1fe2df2d4e7
Ren149 <jeric149@gmail.com>
2016-08-04 12:54:59 +0800

    Newly Created Files

---
+++ ReorderListManager.java
@@ -0,0 +1,14 @@
+package ProjectBackEnd;
+
+import DBConnector.DBConnection;
+
+public class ReorderListManager {
+
+	private DBConnection con;
+
+	public ReorderListManager()
+	{
+		con = new DBConnection();
+	}
+
+}
