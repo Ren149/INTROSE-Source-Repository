66cf4ac801e717876d1bcf3d614ae9f620c56528
Nico Rapisora <Nico Rapisora>
2016-08-13 14:41:52 +0800

    Revert "Completed Expiry List UI"
    This reverts commit 4ae31ff344b172ba2bc41ba1f06c0fcd9f776676.

--- Pharmacy/src/ProjectFrontEnd/ExpiryPanel.java
+++ Pharmacy/src/ProjectFrontEnd/ExpiryPanel.java
@@ -2,14 +2,13 @@
 package ProjectFrontEnd;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Font;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 
+import javax.swing.AbstractAction;
 import javax.swing.ButtonGroup;
-import javax.swing.DefaultComboBoxModel;
-import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
@@ -23,59 +22,48 @@ import ProjectBackEnd.BatchManager;
 import ProjectBackEnd.ProductManager;
 import net.miginfocom.swing.MigLayout;
 
-public class ExpiryPanel extends JPanel implements ItemListener {
+public class ExpiryPanel extends JPanel {
 	private JTable tblExpiryList = new JTable();;
 	private JLabel lblTitle = new JLabel("Expiry List");
 	private JScrollPane scrNearExpiredProducts = new JScrollPane();
 	private final JPanel panel = new JPanel();
 	private final JRadioButton rdbtnThisMonth = new JRadioButton("This month");
-	private final JRadioButton rdbtnWithinTheNext = new JRadioButton("Within the next");
+	private final JRadioButton rdbtnPastMonth = new JRadioButton("Past month");
 	
 	private ButtonGroup buttonGroup = new ButtonGroup();
 	
 	private ProductManager pm = new ProductManager();
 	private BatchManager bm = new BatchManager();
 	private final JLabel lblItemCount = new JLabel("");
-	private final JComboBox comboBox = new JComboBox();
-	private final JComboBox cboMonth = new JComboBox();
-	private final JLabel lblMonths = new JLabel("month");
 	
 	public ExpiryPanel() {
 		setBackground(Color.WHITE);
-		setLayout(new MigLayout("", "[grow][grow]", "[][][grow]"));
+		setLayout(new MigLayout("", "[][grow]", "[][][grow]"));
 		
 		lblTitle.setFont(new Font("Segoe UI Light", Font.PLAIN, 16));
 		
+		add(lblTitle, "cell 0 0");
+		
 		tblExpiryList.setFont(new Font("Segoe UI", Font.PLAIN, 11));
 		tblExpiryList.getTableHeader().setFont(new Font("Segoe UI", Font.PLAIN, 11));
+		lblItemCount.setFont(new Font("Segoe UI", Font.ITALIC, 11));
+		
+		add(lblItemCount, "cell 1 0");
 		
 		scrNearExpiredProducts.setViewportView(tblExpiryList);
-
-		lblItemCount.setFont(new Font("Segoe UI", Font.PLAIN, 9));
-
+		add(scrNearExpiredProducts, "cell 1 1 1 2,grow");
+		
+		buttonGroup.add(rdbtnThisMonth);
 		rdbtnThisMonth.setBackground(Color.WHITE);
 		rdbtnThisMonth.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 11));
 		
-		rdbtnWithinTheNext.setBackground(Color.WHITE);
-		rdbtnWithinTheNext.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 11));
+		add(rdbtnThisMonth, "cell 0 1");
+		buttonGroup.add(rdbtnPastMonth);
+		
+		rdbtnPastMonth.setBackground(Color.WHITE);
+		rdbtnPastMonth.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 11));
 
-		buttonGroup.add(rdbtnThisMonth);
-		buttonGroup.add(rdbtnWithinTheNext);
-		
-		cboMonth.setBackground(Color.WHITE);
-		cboMonth.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"}));
-		cboMonth.setFont(new Font("Segoe UI", Font.PLAIN, 11));
-		cboMonth.addItemListener(this);
-		
-		lblMonths.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 11));
-		
-		add(lblTitle, "cell 0 0 2 1");
-		add(rdbtnThisMonth, "flowx,cell 0 1,growy");
-		add(scrNearExpiredProducts, "cell 0 2 2 1,grow");
-		add(lblItemCount, "cell 1 1,alignx right,aligny bottom");
-		add(rdbtnWithinTheNext, "cell 0 1,growy");
-		add(cboMonth, "cell 0 1,growy");
-		add(lblMonths, "cell 0 1,growy");
+		add(rdbtnPastMonth, "cell 0 2,aligny top");
 		
 		loadExpiryList();
 	}
@@ -129,29 +117,15 @@ public class ExpiryPanel extends JPanel implements ItemListener {
 			
 			int rowCount = tblExpiryList.getRowCount();
 			
-			switch(rowCount) {
+			/*switch(rowCount) {
 				case 0 : lblItemCount.setText("No items displayed."); break;
 				case 1 : lblItemCount.setText("Displaying " + rowCount + " product"); break;
 				default: lblItemCount.setText("Displaying " + rowCount + " products"); break;
-			}
+			}*/
 		}
 	
 	public void update() {
-		loadExpiryList();
+		
 	}
-
-	@Override
-	public void itemStateChanged(ItemEvent e) {
-		// TODO Auto-generated method stub
-		if(e.getSource().equals(cboMonth)) {
-			if (e.getStateChange() == ItemEvent.SELECTED) {
-				if(cboMonth.getSelectedIndex() == 0) {
-					lblMonths.setText("month");
-				}
-				else {
-					lblMonths.setText("months");
-				}
-			}
-		}
-	}
+	
 }
